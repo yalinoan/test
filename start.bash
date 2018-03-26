a81fafc3d25eec61e93fba30fc410a244c25387c
yalinoan <37686364+yalinoan@users.noreply.github.com>
2018-03-26 23:18:24 +0300

    Add files via upload

---
+++ start.bash
@@ -0,0 +1,6 @@
+export GPU_FORCE_64BIT_PTR 0
+export GPU_MAX_HEAP_SIZE 100
+export GPU_USE_SYNC_OBJECTS 1
+export GPU_MAX_ALLOC_PERCENT 100
+export GPU_SINGLE_ALLOC_PERCENT 100
+./nsgpucnminer -xpool ssl://xmr-eu2.nanopool.org:14433 -xwal  4Hm3YrYNgczRAP7jbGCZ7vA8XwbBR8DWMU7Bm9FKZqjxQXPPcwMP1kDbK3mtBSdt2c6TmLCPiMSXa39uBiEBwkg4FVptXWjWL5RCHv7T9ds.the2818 -xpsw z
